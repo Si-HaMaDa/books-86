97ace3eaaa531bf4e2deca0c097f0867a75cc719
Si HaMaDa <nrookman_27@yahoo>
2023-05-21 18:39:01 +0300

    last session

---
+++ resources/views/greeting.blade.php
@@ -0,0 +1,8 @@
+<html>
+
+<body>
+    <h1>Hello, Gretting from View</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci inventore impedit, mollitia odio ratione deserunt totam, labore rem, perspiciatis iusto laboriosam eius libero saepe eaque unde consequatur repudiandae. Deleniti, velit!</p>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::view('/', 'welcome');
+
+
+// Route::redirect('/users', '/greeting');
+
+
+Route::post('greeting', function () {
+    return "Hello Form POST";
 });
+
+Route::get('greeting', function () {
+    return view('greeting');
+});
+
+// Route::get('/user', [UserController::class, 'index']);
+
+// Route::get('/user/create', [UserController::class, 'create']);
+
+// Route::post('/user/create', [UserController::class, 'store']);
+
+Route::get('users/{name}', function (string $name) {
+    return "Hello From User $name";
+});
+
+
+Route::view('/', 'index');
+
+Route::view('/contact', 'contact');
