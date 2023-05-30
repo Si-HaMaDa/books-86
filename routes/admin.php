910231d2a94602bad8be0ba0e39c7c099d2794be
Si HaMaDa <nrookman_27@yahoo>
2023-05-30 22:01:59 +0300

    Use resource route for categories

--- routes/admin.php
+++ routes/admin.php
@@ -13,21 +13,20 @@ Route::group([
     // admin
     Route::view('/', 'admin.index')->name('index');
 
-    Route::controller(CategoryController::class)
-        ->prefix('categories')
-        ->name('categories.')
-        // ->middleware('auth')
-        ->group(function () {
+    // Route::controller(CategoryController::class)
+    //     ->prefix('categories')
+    //     ->name('categories.')
+    //     // ->middleware('auth')
+    //     ->group(function () {
+    //         // admin/categories
+    //         Route::get('/', 'index')->name('index'); // admin.categories.index
+    //         Route::get('create', 'create')->name('create');
+    //         Route::post('/', 'store')->name('store');
+    //         Route::get('/{id}', 'show')->name('show');
+    //         Route::get('/{id}/edit', 'edit')->name('edit');
+    //         Route::put('/{id}', 'update')->name('update');
+    //         Route::delete('/{id}', 'destroy')->name('destroy');
+    //     });
 
-            // admin/categories
-            Route::get('/', 'index')->name('index'); // admin.categories.index
-            Route::get('create', 'create')->name('create');
-            Route::post('/', 'store')->name('store');
-            Route::get('/{id}', 'show')->name('show');
-            Route::get('/{id}/edit', 'edit')->name('edit');
-            Route::put('/{id}', 'update')->name('update');
-            Route::delete('/{id}', 'destroy')->name('destroy');
-        });
-
-    // Route::resource('categories', CategoryController::class);
+    Route::resource('categories', CategoryController::class);
 });
