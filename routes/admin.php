66287676cd4238c57ee11bd95ffcedd09151f762
Si HaMaDa <nrookman_27@yahoo>
2023-06-06 22:25:13 +0300

    Add group attributes to Route Service Provider

--- routes/admin.php
+++ routes/admin.php
@@ -4,39 +4,32 @@ use App\Http\Controllers\Admin\BookController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\HashtagController;
 use App\Http\Controllers\Admin\UserController;
-use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::group([
-    'prefix' => 'admin',
-    'as'     => 'admin.',
-    'middleware' => ['auth', 'admin'],
-], function () {
 
-    // admin
-    Route::view('/', 'admin.index')->name('index');
+// admin
+Route::view('/', 'admin.index')->name('index');
 
-    // Route::controller(CategoryController::class)
-    //     ->prefix('categories')
-    //     ->name('categories.')
-    //     // ->middleware('auth')
-    //     ->group(function () {
-    //         // admin/categories
-    //         Route::get('/', 'index')->name('index'); // admin.categories.index
-    //         Route::get('create', 'create')->name('create');
-    //         Route::post('/', 'store')->name('store');
-    //         Route::get('/{id}', 'show')->name('show');
-    //         Route::get('/{id}/edit', 'edit')->name('edit');
-    //         Route::put('/{id}', 'update')->name('update');
-    //         Route::delete('/{id}', 'destroy')->name('destroy');
-    //     });
+// Route::controller(CategoryController::class)
+//     ->prefix('categories')
+//     ->name('categories.')
+//     // ->middleware('auth')
+//     ->group(function () {
+//         // admin/categories
+//         Route::get('/', 'index')->name('index'); // admin.categories.index
+//         Route::get('create', 'create')->name('create');
+//         Route::post('/', 'store')->name('store');
+//         Route::get('/{id}', 'show')->name('show');
+//         Route::get('/{id}/edit', 'edit')->name('edit');
+//         Route::put('/{id}', 'update')->name('update');
+//         Route::delete('/{id}', 'destroy')->name('destroy');
+//     });
 
-    // Route::resource('categories', CategoryController::class);
+// Route::resource('categories', CategoryController::class);
 
-    Route::resources([
-        'categories' => CategoryController::class,
-        'hashtags'   => HashtagController::class,
-        'books'      => BookController::class,
-        'users'      => UserController::class,
-    ]);
-});
+Route::resources([
+    'categories' => CategoryController::class,
+    'hashtags'   => HashtagController::class,
+    'books'      => BookController::class,
+    'users'      => UserController::class,
+]);
